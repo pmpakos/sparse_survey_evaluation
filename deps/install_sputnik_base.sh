0d0aa3336006107837f6e19333521944456c9558
pmpakos <pmpakos@hotmail.com>
2025-05-18 18:04:06 +0300

    Fix installation script of sputnik to run on epyc6 too.

--- deps/install_sputnik_base.sh
+++ deps/install_sputnik_base.sh
@@ -3,9 +3,15 @@
 cd ./sputnik
 git reset --hard; git clean -fd
 
+# Need to change this for epyc6, because otherwise it tries to compare a long int with an int...
+sed -i '/std::max(SIGSTKSZ, 65536)/ s/65536/(long int) 65536/' third_party/abseil-cpp/absl/debugging/failure_signal_handler.cc
+
 # First, set the PATH variable accordingly to use specific gcc and cmake
 export PATH=/various/dgal/gcc/gcc-12.2.0/gcc_bin/bin:$PATH
 export PATH=/various/pmpakos/epyc5_libs/cmake-3.26.0-rc3/build/bin/:$PATH
+
+# I had forgot about it, libssl of this version is not on epyc6 pre-installed...
+export LD_LIBRARY_PATH=/various/pmpakos/epyc5_libs/openssl-1.1.1o/:$LD_LIBRARY_PATH
 
 mkdir build
 cd build
